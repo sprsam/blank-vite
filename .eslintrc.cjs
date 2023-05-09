d598b01d408fd16051be9963bb07110cdc6202b5
Sam <sam.tietze@spr.com>
2023-05-09 11:49:08 -0500

    install tailwind; comment out default stylesheets; genericize things for template; try to fix up eslint rules
    configure tsconfig for eslint; run lint fixes for each file
    
    update some linter rules for ease
    
    update to create template
    
    Update name for blank template

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,16 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', project: 'tsconfig.eslint.json', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    animation: {
+      'logo-spin': 'logo-spin infinite 20s linear',
+    },
+    extend: {
+      keyframes: {
+        'logo-spin': {
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
